d1f236c3046e74257ef54e5c1a74e5a00e020ea4
209295 <bartlomiejdettlaff@interia.pl>
2019-10-08 17:29:13 +0200

    Pierwszy commit

---
+++ src/main/java/com/dettlaff/zadanie2/Zadanie2Application.java
@@ -0,0 +1,13 @@
+package com.dettlaff.zadanie2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zadanie2Application.class, args);
+	}
+
+}
